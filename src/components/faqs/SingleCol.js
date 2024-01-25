ee694ad96f40f325154266d4eb3c79ed816fb16c
0Suryansh <suryanshsrv@gmail.com>
2024-01-25 11:19:21 +0530

    base commit

---
+++ src/components/faqs/SingleCol.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
+import { SectionDescription } from "components/misc/Typography.js";
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
+import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-7.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-8.svg";
+
+const Subheading = tw(SubheadingBase)`mb-4 text-center`;
+const Heading = tw(SectionHeading)`w-full`;
+const Description = tw(SectionDescription)`w-full text-center`;
+
+const Column = tw.div`flex flex-col items-center`;
+const HeaderContent = tw.div``;
+
+const FAQSContainer = tw.dl`mt-12 max-w-4xl relative`;
+const FAQ = tw.div`cursor-pointer select-none mt-5 px-8 sm:px-10 py-5 sm:py-4 rounded-lg text-gray-800 hover:text-gray-900 bg-gray-200 hover:bg-gray-300 transition duration-300`;
+const Question = tw.dt`flex justify-between items-center`;
+const QuestionText = tw.span`text-lg lg:text-xl font-semibold`;
+const QuestionToggleIcon = motion(styled.span`
+  ${tw`ml-2 transition duration-300`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`);
+const Answer = motion(tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`);
+
+const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+  ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400`}
+`;
+const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
+  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-15 transform -translate-x-2/3 text-primary-500`}
+`;
+
+
+
+export default ({
+  subheading = "FAQS",
+  heading = "You have Questions ?",
+  description = "And we have got answers to all of them.",
+  questions = [
+    {
+      question: "How can I benefit from connecting with industrial professionals through your platform?",
+      answer:
+        "Our platform facilitates networking and collaboration within the industrial sector, providing opportunities for business growth, knowledge exchange, and staying abreast of industry trends."
+    },
+    {
+      question: "What types of professionals can I connect with on your platform?",
+      answer:
+        "Our community includes a diverse range of industrial experts, engineers, innovators, and business leaders. Whether you're seeking partnerships, advice, or simply expanding your network, you'll find valuable connections here."
+    },
+    {
+      question: "How can I ensure the privacy and security of my information on the platform?",
+      answer:
+        "We prioritize the security of your data. Our platform employs robust encryption measures, and you have control over the visibility of your profile and the information you share with the community.."
+    },
+    {
+      question: "Can I participate in industry-specific discussions and forums?",
+      answer:
+        "Absolutely! Engage in vibrant discussions, share insights, and stay informed about the latest industry developments through our specialized forums and discussion groups."
+    }
+  ]
+}) => {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
+
+  const toggleQuestion = questionIndex => {
+    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
+    else setActiveQuestionIndex(questionIndex);
+  };
+
+  return (
+    <Container>
+      <ContentWithPaddingXl>
+        <Column>
+          <HeaderContent>
+            {subheading && <Subheading>{subheading}</Subheading>}
+            <Heading>{heading}</Heading>
+            {description && <Description>{description}</Description>}
+          </HeaderContent>
+          <FAQSContainer>
+            {questions.map((faq, index) => (
+              <FAQ
+                key={index}
+                onClick={() => {
+                  toggleQuestion(index);
+                }}
+                className="group"
+              >
+                <Question>
+                  <QuestionText>{faq.question}</QuestionText>
+                  <QuestionToggleIcon
+                    variants={{
+                      collapsed: { rotate: 0 },
+                      open: { rotate: -180 }
+                    }}
+                    initial="collapsed"
+                    animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                    transition={{ duration: 0.02, ease: [0.04, 0.62, 0.23, 0.98] }}
+                  >
+                    <ChevronDownIcon />
+                  </QuestionToggleIcon>
+                </Question>
+                <Answer
+                  variants={{
+                    open: { opacity: 1, height: "auto", marginTop: "16px" },
+                    collapsed: { opacity: 0, height: 0, marginTop: "0px" }
+                  }}
+                  initial="collapsed"
+                  animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                  transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                >
+                  {faq.answer}
+                </Answer>
+              </FAQ>
+            ))}
+          </FAQSContainer>
+        </Column>
+      </ContentWithPaddingXl>
+      <DecoratorBlob1/>
+      <DecoratorBlob2 />
+    </Container>
+  );
+};
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,92 @@
+import React from "react";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import tw from "twin.macro";
+import styled from "styled-components"; //eslint-disable-line
+import { css } from "styled-components/macro"; //eslint-disable-line
+import Header from "components/headers/light.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+import MainFeature1 from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
+import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
+import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
+
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats.js";
+// import MainFeature3 from "components/features/TwoColSingleFeatureWithStats2.js";
+import Features from "components/features/ThreeColSimple.js";
+// import Features from "components/features/ThreeColWithSideImage.js";
+import TeamCardGrid from "components/cards/ProfileThreeColGrid.js";
+
+import SupportIconImage from "images/support-icon.svg";
+import ShieldIconImage from "images/shield-icon.svg";
+import CustomerLoveIconImage from "images/simple-icon.svg";
+
+const Subheading = tw.span`uppercase tracking-wider text-sm`;
+const HighlightedText = tw.span`text-primary-500`;
+
+export default () => {
+  return (
+    <AnimationRevealPage>
+      <Header />
+      <MainFeature2
+        subheading={<Subheading>VALUES</Subheading>}
+        heading={
+          <>
+            We Always Abide by Our <HighlightedText>Principles.</HighlightedText>
+          </>
+        }
+        imageSrc={prototypeIllustrationImageSrc}
+        showDecoratorBlob={false}
+        features={[
+          {
+            Icon: MoneyIcon,
+            title: "Affordable",
+            description: "We have the best professional across all major industries and sector to connect with.",
+            iconContainerCss: tw`bg-green-300 text-green-800`
+          },
+          {
+            Icon: BriefcaseIcon,
+            title: "Professionalism",
+            description: "We assure you that our templates are designed and created by professional designers.",
+            iconContainerCss: tw`bg-red-300 text-red-800`
+          }
+        ]}
+      />
+      <MainFeature1
+        subheading={<Subheading>Our Vision</Subheading>}
+        heading="We aim to disrupt the design space."
+        buttonRounded={false}
+        primaryButtonText="Contact Us"
+        imageSrc="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=768&q=80"
+        textOnLeft={false}
+      />
+      {/* <Features
+        subheading={<Subheading>Our Values</Subheading>}
+        heading="We follow these."
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        cards={[
+          {
+            imageSrc: SupportIconImage,
+            title: "24/7 Support",
+            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+          },
+          {
+            imageSrc: ShieldIconImage,
+            title: "Strong Teams",
+            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+          },
+          {
+            imageSrc: CustomerLoveIconImage,
+            title: "Customer Satisfaction",
+            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+          },
+        ]}
+        linkText=""
+      /> */}
+      {/* <TeamCardGrid 
+        subheading={<Subheading>Our Team</Subheading>}
+      /> */}
+      {/* <Footer /> */}
+    </AnimationRevealPage>
+  );
+};
